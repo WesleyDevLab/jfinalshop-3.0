3c2a0ee8703fe1c697bc1079027e341621c7e413
江南红衣 <hycx227@163.com>
2016-10-14 20:27:05 +0800

    Signed-off-by: 江南红衣 <hycx227@163.com>

---
+++ jfinalshop-web/src/main/java/com/jfinalshop/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.jfinalshop.controller;
+
+import com.jfinal.core.Controller;
+
+public class TestController extends Controller{
+
+	public void index() {
+		renderText("Hello World!");
+	}
+}
